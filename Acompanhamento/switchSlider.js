60bb7b5d0d75a17878a11db2c2c69736701f3f4a
lucasnoveline <lucasnoveline@gmail.com>
2018-06-20 00:25:12 -0300

    Add files via upload

---
+++ Acompanhamento/switchSlider.js
@@ -0,0 +1,38 @@
+function switchSlider () {
+	// Coletando valor do slider
+	var slider = document.getElementById("slider-switch");
+	var output = document.getElementById("data-switch");
+	var val = slider.value;
+
+	// Convertendo em data
+	if (val < 17) {
+		var dia = Number(val) + 14;
+		var data = '06.' + dia;
+		var show = dia + ' de Junho';
+	}
+	else {
+		var dia = Number(val) - 16;
+		var data = '07.' + dia;
+		var show = dia + ' de Julho';
+	}
+	output.innerHTML = show;
+
+	// Caso de click no Slider
+	slider.oninput = function() {
+		var val = this.value;
+		if (val < 17) {
+			var dia = Number(val) + 14;
+			var data = '06.' + dia;
+			var show = dia + ' de Junho';
+		}
+		else {
+			var dia = Number(val) - 16;
+			var data = '07.' + dia;
+			var show = dia + ' de Julho';
+		}
+		output.innerHTML = show;
+		// Modificando tabela de grupos
+		var switch = document.getElementByClassName("switch");
+		var frame = "<iframe width='600' height='1200' frameborder='0' scrolling='yes' src='https://lucasnoveline.github.io/worldcup2018_posts/Acompanhamento/Switch/?data=" + data + "' align='middle'></iframe>";
+		switch.innerHTML = frame;
+}
