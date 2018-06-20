39d5b6732c5ffcf4e6096b6489a6cd05a0a83513
lucasnoveline <lucasnoveline@gmail.com>
2018-06-20 00:29:25 -0300

    Update groupStageSliders.js

--- Acompanhamento/groupStageSliders.js
+++ Acompanhamento/groupStageSliders.js
@@ -39,6 +39,7 @@ function groupStageSliders () {
 		for (i = 0; i < tables.length; i++) { 
 		    var group = tables[i].id;
 		    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/worldcup2018_posts/Acompanhamento/Tabelas_grupos/?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
+		    console.log(frame);
 		    tables[i].innerHTML = frame;
 		}
 }
