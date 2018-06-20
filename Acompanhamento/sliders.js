116177f4d1016302ace4fb9c04bb1f471d3566ae
lucasnoveline <lucasnoveline@gmail.com>
2018-06-20 00:04:48 -0300

    Update sliders.js

--- Acompanhamento/sliders.js
+++ Acompanhamento/sliders.js
@@ -39,20 +39,45 @@ function groupStageSliders () {
 		for (i = 0; i < tables.length; i++) { 
 		    var group = tables[i].id;
 		    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/tables_test2?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
-		    console.log(frame);
 		    tables[i].innerHTML = frame;
 		}
-		}
+}
 
+function switchSlider () {
+	// Coletando valor do slider
+	var slider = document.getElementById("slider-switch");
+	var output = document.getElementById("data-switch");
+	var val = slider.value;
+
+	// Convertendo em data
+	if (val < 17) {
+		var dia = Number(val) + 14;
+		var data = '06.' + dia;
+		var show = dia + ' de Junho';
+	}
+	else {
+		var dia = Number(val) - 16;
+		var data = '07.' + dia;
+		var show = dia + ' de Julho';
+	}
+	output.innerHTML = show;
+
+	// Caso de click no Slider
+	slider.oninput = function() {
+		var val = this.value;
+		if (val < 17) {
+			var dia = Number(val) + 14;
+			var data = '06.' + dia;
+			var show = dia + ' de Junho';
+		}
+		else {
+			var dia = Number(val) - 16;
+			var data = '07.' + dia;
+			var show = dia + ' de Julho';
+		}
+		output.innerHTML = show;
 		// Modificando tabela de grupos
-		var tables = document.getElementsByClassName("tables");
-		var i;
-
-		// Para cada tabela de grupos
-		for (i = 0; i < tables.length; i++) { 
-		    var group = tables[i].id;
-		    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/tables_test2?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
-		    console.log(frame);
-		    tables[i].innerHTML = frame;
-		}
+		var switch = document.getElementByClassName("switch");
+		var frame = "<iframe width='600' height='1200' frameborder='0' scrolling='yes' src='https://lucasnoveline.github.io/worldcup2018_posts/Acompanhamento/Switch/?data=" + data + "' align='middle'></iframe>";
+		switch.innerHTML = frame;
 }
